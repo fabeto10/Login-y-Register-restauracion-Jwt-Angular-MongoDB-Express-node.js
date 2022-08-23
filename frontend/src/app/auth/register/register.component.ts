4e0d7d33d0029cab924d689e3572a979a27ebf14
Francisco Vidal <f0414475@gmail.com>
2022-08-22 22:10:42 -0400

    register.component.ts creando

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserI } from 'src/app/models/user';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+  onRegister(form: { value: UserI; }): void{
+    this.authService.register(form.value).subscribe(res =>{
+      this.router.navigateByUrl('./auth');
+    });
   }
 
 }
